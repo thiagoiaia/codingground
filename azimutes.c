04addc8a1d0d89922fca69464c30d7857a9e4934
thiagosape <thiago-sape@openmailbox.org>
2015-03-20 00:56:36 -0300

    Update azimutes.c

--- azimutes.c
+++ azimutes.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
-
-
 float ConverterSexagesimalEmDecimal(float grau, float min, float seg)
 {
     //conversão de segundos em minutos, somados aos já minutos existentes.
     min = min + seg / 60;
     //Conversão de minutos em graus, somados aos já graus existentes.
     grau = grau + min / 60;
-
     return grau;
 }
 
 int main()
 {
+    float AngulosHorizontaisEmDecimal[6];
     float grau, minuto, segundo;
-    printf("Entre com o grau da coordenada: "); scanf("%f",&grau);
-    printf("Entre com o minuto da coordenada: "); scanf("%f",&minuto);
-    printf("Entre com o segundo da coordenada: "); scanf("%f",&segundo);
     
-    float resultado = ConverterSexagesimalEmDecimal(grau, minuto, segundo);
-
-    printf("%f",resultado);
+    int i;
+    for(i = 0; i < 6; i++)
+    {
+        printf("Entre com o grau do angulo %d: ", i); scanf("%f",&grau);
+        printf("Entre com o minuto do angulo %d: ", i); scanf("%f",&minuto);
+        printf("Entre com o segundo do angulo %d: ", i); scanf("%f",&segundo);
+        
+        AngulosHorizontaisEmDecimal[i] = ConverterSexagesimalEmDecimal(grau, minuto, segundo);
+    }
+    
+    for(i = 0; i < 6; i++)
+    {
+        printf("%f\n",AngulosHorizontaisEmDecimal[i]);
+    }
     
     return 0;
 }
